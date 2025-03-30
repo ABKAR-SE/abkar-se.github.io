24faf71447ea5b85c45ce1a3b0788338ef3a8e78
Libaak SE <147570822+Libaax1998@users.noreply.github.com>
2025-03-30 18:35:46 +0200

    overhaul

--- script.js
+++
@@ -1,9 +0,0 @@
-function openStripeWindow(url) {
-    console.log("Apertura finestra di Stripe:", url);
-    var newWindow = window.open(url, "_blank");
-    if (newWindow) {
-        newWindow.opener = null;
-    } else {
-        window.location.href = url;
-    }
-}
